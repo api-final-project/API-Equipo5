28f403e24d8afd8da0ef8a4a4f1bd9ed98728e4c
Dmogrsap <72819804+Dmogrsap@users.noreply.github.com>
2020-11-30 18:34:03 -0700

    Update main.py

--- main.py
+++ main.py
@@ -17,3 +17,42 @@ post_boss_args.add_argument('stage_hp',type=int,help="Error Stage HP is required
 post_boss_args.add_argument('rooms_appear',type=str,help="Error Room Appear is required",required=True)
 post_boss_args.add_argument('image',type=str,help="Error image is required",required=True)
 post_boss_args.add_argument('video',type=str,help="Error video is required",required=True)
+
+class Test(Resource):
+    def get(self):
+        return jsonify({"message":"API working ok"})
+
+class Bosse(Resource):
+    def get(self,id):
+        self.abort_if_not_exist(id)
+        boss = db.db.bosses_binding_of_isaac.find_one({'id':id})
+        del boss['_id']
+        return jsonify(boss)
+    
+    def post(self):
+        self.abort_if_id_exist(request.json['id'])
+        args = post_boss_args.parse_args()
+        db.db.bosses_binding_of_isaac.insert_one({
+            'id':request.json['id'],
+            'name':request.json['name'],
+            'base_hp':request.json['base_hp'],
+            'stage_hp':request.json['stage_hp'],
+            'rooms_appear':request.json['rooms_appear'],
+            'image':request.json['image'],
+            'video':request.json['video']
+        })
+        return jsonify({"create":request.json})
+
+    def put(self,id):
+        self.abort_if_not_exist(id)
+        db.db.bosses_binding_of_isaac.update_one({'id':id},
+        {'$set':{
+            'id':request.json['id'],
+            'name':request.json['name'],
+            'base_hp':request.json['base_hp'],
+            'stage_hp':request.json['stage_hp'],
+            'rooms_appear':request.json['rooms_appear'],
+            'image':request.json['image'],
+            'video':request.json['video']
+        }})
+        return jsonify({"update":request.json})
